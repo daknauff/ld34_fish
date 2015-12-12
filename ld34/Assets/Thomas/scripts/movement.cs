e409c7162483bf00e0c65c031ce44b71216a888b
Thomastert <thomas@thoemda.com>
2015-12-12 13:30:24 +0100

    player movement
    en screen border

---
+++ ld34/Assets/Thomas/scripts/movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+    public float speed = 1.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+	}
+}
