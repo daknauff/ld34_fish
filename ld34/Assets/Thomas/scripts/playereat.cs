36dad20d4098a95de9c38188c0136961d34cdb01
Thomastert <thomas@thoemda.com>
2015-12-12 19:29:49 +0100

    music and bomb start

--- ld34/Assets/Thomas/scripts/playereat.cs
+++ ld34/Assets/Thomas/scripts/playereat.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class playereat : MonoBehaviour {
-    
-    
+    public AudioClip gulp;
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
     void OnCollisionEnter2D(Collision2D col)
     {
         float newscale = transform.localScale.x;
         if (col.transform.localScale.x <= transform.localScale.x) 
         {
-
+            audio.PlayOneShot(gulp, 10F);
             Destroy(col.gameObject);
             newscale++;
             transform.localScale = new Vector3(newscale, newscale, newscale);
