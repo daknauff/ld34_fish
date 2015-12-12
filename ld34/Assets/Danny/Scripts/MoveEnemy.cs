9ea7eb48fb06ba7169dd9896fa2c51abcb18dbaa
daknauff <gidhub@thoemda.com>
2015-12-12 15:45:52 +0100

    variospeed

--- ld34/Assets/Danny/Scripts/MoveEnemy.cs
+++ ld34/Assets/Danny/Scripts/MoveEnemy.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class MoveEnemy : MonoBehaviour {
 	public int movespeed;
+	float variospeed;
 
 	// Use this for initialization
 	void Start () {
+		variospeed=Random.Range(1,3);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float x_auto = Time.deltaTime*movespeed;
+		float x_auto = Time.deltaTime*(movespeed*variospeed);
+		Debug.Log(x_auto);
 		transform.Translate(x_auto,0,0);
 	
 	}
