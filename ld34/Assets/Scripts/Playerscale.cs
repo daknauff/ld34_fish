e96854bbf6cbbcc442a4d018db87572638145fa5
Danny <gidhub@thoemda.com>
2015-12-13 21:17:00 +0100

    remove sub drop bombs

--- ld34/Assets/Scripts/Playerscale.cs
+++ ld34/Assets/Scripts/Playerscale.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public static class Playerscale {
 	public static float Scale=0.25f;
-	public static int level=1;
+	public static int level=4;
     public static string scene = "Level1";
     public static int speedincrease = 0;
 
     public static void reset()
     {
         Scale = 0.25f;
-        level = 1;
+        level = 4;
         scene = "Level1";
     }
     public static void swapscene()
