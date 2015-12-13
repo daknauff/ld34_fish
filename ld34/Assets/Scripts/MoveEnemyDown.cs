62b74381609e9f11cf3a541f76e516221d0e3d85
Thomastert <thomas@thoemda.com>
2015-12-13 20:40:20 +0100

    submarine bomb drop + fix bomb flaw

---
+++ ld34/Assets/Scripts/MoveEnemyDown.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveEnemyDown : MonoBehaviour {
+    public int movespeed;
+    float variospeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        variospeed = Random.Range(1, 3);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x_auto = Time.deltaTime * (movespeed * variospeed);
+        transform.Translate(x_auto, x_auto, 0);
+
+    }
+    void OnBecameInvisible()
+    {
+
+        Destroy(gameObject);
+    }
+}
---
+++ ld34/Assets/Scripts/SubBoms.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubBoms : MonoBehaviour {
+    public GameObject bomb;
+    private float fireSpellStart = 0f;
+    private float fireSpellCooldown = 3f; // 2 = two seconds 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Drop () {
+        
+       Instantiate(bomb, transform.position, Quaternion.identity);
+	}
+
+    void Update() 
+    {
+
+        if (Time.time > fireSpellStart + fireSpellCooldown)
+        {
+            fireSpellStart = Time.time;
+
+            Drop();
+        }
+    }
+}
